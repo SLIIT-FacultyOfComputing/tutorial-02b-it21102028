08c6a907bc6241717c33a38f572da220794396ef
IT21102028 <87367406+it21102028@users.noreply.github.com>
2021-07-20 10:53:39 +0000

    All Questions Completed

--- tute04.cpp
+++ tute04.cpp
@@ -11,19 +11,50 @@ long Factorial(int no);
 long nCr(int n, int r);
 Do not modify the main function.*/
 
-#include <iostream>
+#include<iostream>
+
+using namespace std;
 
 long Factorial(int no);
 long nCr(int n, int r);
 
-int main() {
-  int n, r;
-  std::cout << "Enter a value for n ";
-  std::cin >> n;
-  std::cout << "Enter a value for r ";
-  std::cin >> r;
-  std::cout << "nCr = ";
-  std::cout << nCr(n,r);
-  std::cout << std::endl;
-  return 0;
+int main ()
+{
+  
+  int n;
+  int r;
+  
+cout<<"Enter a value for n\t:";
+cin>>n;
+cout<<"Enter a value for r\t:";
+cin>>r;
+cout<<"nCr value\t:"<<Factorial(n)/nCr(n,r)<<endl;
+
+exit(0);
+}
+
+long Factorial(int no)
+{
+  int row;
+  int n=1;
+  for(row=1;row<=no;row++)
+  {
+    n =  n * row;
+  }
+  return n;
+}
+long nCr(int n, int r)
+{
+  int row;
+  int first = 1;
+  int secound = 1;
+  for(row=1;row<=r;row++)
+  {
+    first = first*row;
+  }
+  for(row=1;row<=(n-r);row++)
+  {
+    secound=secound*row;
+  }
+  return first*secound;
 }
